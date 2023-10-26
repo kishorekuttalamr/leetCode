8c28ca046975d2318e6d895672acc4fd3a30cd2f
kishorekuttalamr <142158625+kishorekuttalamr@users.noreply.github.com>
2023-10-26 23:06:37 +0530

    Create twosum.cpp

---
+++ twosum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+      vector<int> result(2, 0); 
+
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result; 
+                }
+            }
+        }
+
+        
+        return result;
+    }
+};
